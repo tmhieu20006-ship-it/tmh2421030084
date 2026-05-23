d5e68cd6e2487096861d07d044de90e81a4eddb6
tmhieu20006-ship-it <tmhieu20006@gmail.com>
2026-05-23 16:22:25 +0700

    Delete chương 3 directory

--- chương 3/chapter 3.3.py
+++
@@ -1,5 +0,0 @@
-strl = "VD ham lstrip() Trong Python"
-print(strl.lstrip("VD ham lstrip() Trong Python"))
-
-strl = "VD ham lstrip() Trong Python"
-print(strl.lstrip("VD ham lstrip() Trong Python"))
--- chương 3/chapter 3.5.py
+++
@@ -1,6 +0,0 @@
-def msg():
-    a = 10
-    print ("Gia tri cua a la: ", a)
-    return
-msg()
-print 
