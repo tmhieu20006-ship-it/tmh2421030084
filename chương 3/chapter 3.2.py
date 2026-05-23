cb311ecb02f65bdd1954de6336c45b4579c5b25d
tmhieu20006-ship-it <tmhieu20006@gmail.com>
2026-05-23 16:30:48 +0700

    Delete chương 3 directory

--- chương 3/chapter 3.2.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Nhap n: "))
-a = []
-for i in range(n):
-    x = int(input("Nhap so: "))
-    a.append(x)
-open = 0
-for i in a:
-    while open < n:
-        print(a[open])
--- chương 3/chapter 3.6.py
+++
@@ -1,8 +0,0 @@
-b=20
-def msg():
-    a = 10
-    print ("Gia tri cua a la",a)
-    print ("Gia tri cua b la",b)
-    return
-msg()
-print (b)
